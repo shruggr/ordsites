ed5143918865ae2bc24a632169ac9c2430a05b06
David Case <david@fyxgaming.com>
2023-11-07 21:32:50 -0500

    update root

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// serve up static files from the public folde
+// serve up static files from the public folder
+app.use('/public', express.static('public'));
 
 // serve up the index.html file
-app.get('/', express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 // start the server
 const port = process.env.PORT || 8080;
